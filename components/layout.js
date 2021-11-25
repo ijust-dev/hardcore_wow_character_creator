9dfb58a5edcf75636c611485213757da17f539b6
jbarrett95 <jakewbarrett@gmail.com>
2021-11-25 14:34:03 -0600

    initial commit

---
+++ components/layout.js
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => {
+    return (
+        <div className="content bg-black w-full">
+            { children }
+        </div>
+    );
+}
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Layout from '../components/layout'
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
